92881ab642462cf547b9315624a36200400b31a4
slaclau <seb.laclau@gmail.com>
2023-02-04 23:54:01 +0000

    fix(setup.py): import data files

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     author_email='seb.laclau@gmail.com',
     #package_dir={'fortiusant':'fortiusant'},
     packages=['fortius_ant'],
+    package_data={
+        'fortius_ant':['./*'],
+    },
     entry_points={
         'console_scripts': [
             'fortius-ant=fortius_ant.FortiusAnt:main',
