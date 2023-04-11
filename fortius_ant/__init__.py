d68f40ca9ac7ebd1590ad988fe99c84e4dd4a008
slaclau <seb.laclau@gmail.com>
2023-04-11 19:22:27 +0100

    feat(__init__.py): add code to read packaging type and version

--- fortius_ant/__init__.py
+++ fortius_ant/__init__.py
@@ -32,3 +32,17 @@ __all__ = [
 from . import _version
 __version__ = _version.get_versions()['version']
 __shortversion__ = _version.get_versions()['version'].split("+")[0]
+__packageversion__ = ""
+__packagetype__ = ""
+try:
+    from . import _snapversion
+    __packageversion__ = _snapversion.__version__
+    __packagetype__ = "snap"
+except:
+    pass
+try:
+    from . import _debversion
+    __packageversion__ = _debversion.__version__
+    __packagetype__ = "deb"
+except:
+    pass
