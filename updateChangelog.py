602c8c6a4193e4d94d347cb623c437d599bf08a8
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-11 15:16:12 +0100

    Update updateChangelog.py

--- updateChangelog.py
+++ updateChangelog.py
@@ -10,9 +10,9 @@ ubuntu_version = subprocess.check_output(["git", "describe", "--tags", "--always
         "--match", f"[[:digit:]]*"]).strip().decode("utf-8")
 ubuntu_tag = subprocess.check_output(["git", "describe", "--tags", "--always", "--abbrev=0",
         "--match", f"[[:digit:]]*"]).strip().decode("utf-8")
-print(ubuntu_tag)
+
 for line in fileinput.input("debian/changelog", inplace = True):
     print(line.replace(") replaceme", "~" + codename + ") " + codename), end = "")
 
 for line in fileinput.input("debian/changelog", inplace = True):
-    print(line.replace(") ubuntutag", ubuntu_version), end = "")
+    print(line.replace(ubuntu_tag, ubuntu_version), end = "")
