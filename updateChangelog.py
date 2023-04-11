67466e2e0f8341a246a74c3a138fc4b69e31fd52
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-11 13:54:17 +0100

    Update updateChangelog.py

--- updateChangelog.py
+++ updateChangelog.py
@@ -1,7 +1,12 @@
 import distro
 import fileinput
+import subprocess
 
 codename = distro.codename()
 
 for line in fileinput.input("debian/changelog", inplace = True):
     print(line.replace(") replaceme", "~" + codename + ") " + codename), end = "")
+
+version = subprocess.check_output(["git", "describe", "--tags", "--dirty", "--always", "--long",
+        "--match", f"{tag_prefix}[[:digit:]]*", "--exclude", "*ubuntu*"]).strip()
+disp(version)
