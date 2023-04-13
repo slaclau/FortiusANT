ff05b910aa13a8d86c44398a55ecce334930c814
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-13 09:16:23 +0100

    Update deptry2md.py

--- scripts/deptry2md.py
+++ scripts/deptry2md.py
@@ -3,8 +3,12 @@ import sys
 
 with open(sys.argv[1]) as f:
     data = json.load(f)
+    if length(sys.argv) > 1:
+        print(f"#{sys.argv[2]}"
     for i in data:
-        print(f"# {i.capitalize()} dependencies:")
+        if i == "misplaced_dev":
+            i = "misplaced development
+        print(f"## {i.capitalize()} dependencies:")
         for j in data[i]:
             print(j)
     
