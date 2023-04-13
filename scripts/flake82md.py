b059272709d105b4d14e9fc29fee1ee69f12594a
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-13 12:04:11 +0100

    Update flake82md.py

--- scripts/flake82md.py
+++ scripts/flake82md.py
@@ -12,6 +12,7 @@ with open(sys.argv[1]) as f:
             count = 0
             for line in data[i]:
                 count += 1
-                print(f'{line["code"]} :{line["line_number"]}:{line["column_number"]}: {line["text"]} [{line["physical_text"]}]')
+                print(f'{line["code"]} :{line["line_number"]}:{line["column_number"]}: {line["text"]} [{line["physical_line"]}]')
                 if count > 100:
+                    print(f"Truncated at {count} errors")
                     break
