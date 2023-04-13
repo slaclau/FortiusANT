e0d391b3d31bcf33ffef6e42f2161fa71a8530e3
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-13 11:44:24 +0100

    Update flake82md.py

--- scripts/flake82md.py
+++ scripts/flake82md.py
@@ -12,6 +12,7 @@ with open(sys.argv[1]) as f:
             count = 0
             for line in data[i]:
                 count += 1
-                print(f"{line[code]}:{line[line_number]}:{line[column_number]}: {line[text]}")
+                if line[code]:
+                    print(f"{line[code]}:{line[line_number]}:{line[column_number]}: {line[text]}")
                 if count > 100:
                     break
