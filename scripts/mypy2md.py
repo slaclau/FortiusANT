8042d1d9a44f05e2e55ba74beef32eaee9496dc0
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-13 18:49:46 +0100

    Update mypy2md.py

--- scripts/mypy2md.py
+++ scripts/mypy2md.py
@@ -7,7 +7,7 @@ with open(sys.argv[1]) as f:
         print(f"# {sys.argv[2]}")
     n = 0
     for i in data:
-        for line in i:
+        for line in data[i]:
             n += data[i][line]
     print(f"{len(data)} files checked, {n} issues found")
     for i in data:
