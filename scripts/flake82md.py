6968459db71fb44c0e4936af493dc483516295ae
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-13 10:44:33 +0100

    Create flake82md.py

---
+++ scripts/flake82md.py
@@ -0,0 +1,12 @@
+import json
+import sys
+
+with open(sys.argv[1]) as f:
+    data = json.load(f)
+    if len(sys.argv) > 1:
+        print(f"## {sys.argv[2]}")
+    for i in data:
+        if len(data[i]) > 0:
+            print(f"## {i.capitalize():")
+            for j in data[i]:
+                print(j)
