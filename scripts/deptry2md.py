1dc2e17760ad7d7e2dd12e844d45139a488545ac
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-13 09:10:27 +0100

    Update deptry2md.py

--- scripts/deptry2md.py
+++ scripts/deptry2md.py
@@ -4,7 +4,7 @@ import sys
 with open(sys.argv[1]) as f:
     data = json.load(f)
     for i in data:
-        print(f"# {i.capitalize()}:")
+        print(f"# {i.capitalize()} dependencies:")
         for j in data[i]:
             print(j)
     
