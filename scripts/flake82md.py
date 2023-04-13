895f1f947318ef68dd358dbae8df381609b4d574
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-13 13:46:58 +0100

    Update flake82md.py

--- scripts/flake82md.py
+++ scripts/flake82md.py
@@ -5,10 +5,14 @@ with open(sys.argv[1]) as f:
     data = json.load(f)
     if len(sys.argv) > 1:
         print(f"# {sys.argv[2]}")
+    n = 0
+    for i in data:
+        n += len(data[i])
+    print(f"{len(data)} files checked, {n} issues found")
     for i in data:
         if len(data[i]) > 0:
             print(f"## {i.capitalize()}:")
-            print(f"{len(data[i])} errors found")
+            print(f"{len(data[i])} issues found")
             count = 0
             for line in data[i]:
                 count += 1
