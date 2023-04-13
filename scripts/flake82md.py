1a824ced3a21f779826687b5da77d9c74175ff71
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-13 11:11:06 +0100

    Update flake82md.py

--- scripts/flake82md.py
+++ scripts/flake82md.py
@@ -5,8 +5,12 @@ with open(sys.argv[1]) as f:
     data = json.load(f)
     if len(sys.argv) > 1:
         print(f"## {sys.argv[2]}")
+    count = 0;
     for i in data:
+        count ++
         if len(data[i]) > 0:
             print(f"## {i.capitalize()}:")
             for j in data[i]:
                 print(j)
+        if count > 100:
+            break
