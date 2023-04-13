2d3fec337cefd3cdfa928255179133ab105739a9
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-13 15:59:01 +0100

    Update flake82md.py

--- scripts/flake82md.py
+++ scripts/flake82md.py
@@ -14,7 +14,12 @@ with open(sys.argv[1]) as f:
             print(f"## {i.capitalize()}:")
             print(f"{len(data[i])} issues found")
             print("The following code(s) occurred in this file")
-            print( set(d["code"] for d in data[i]) )
+            codes = set(d["code"] for d in data[i])
+            codeCount = {}
+            for line in data[i]:
+                codeCount[line['code']] = codeCount.get(line['code'], 0) + 1
+            for code in codes:
+                print(f"{code}: {codeCount[code]} times")
             count = 0
             for line in data[i]:
                 count += 1
