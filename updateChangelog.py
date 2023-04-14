07c320cb2f5a02fa55e7a6f7f1a778d9f594076a
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-14 11:50:41 +0100

    Update updateChangelog.py

--- updateChangelog.py
+++ updateChangelog.py
@@ -4,9 +4,9 @@ import subprocess
 
 codename = distro.codename()
 
-version = subprocess.check_output(["git", "describe", "--tags", "--always", "--long",
+version = subprocess.check_output(["git", "describe", "--tags", "--always",
         "--match", f"[[:digit:]]*", "--exclude", "*ubuntu*"]).strip().decode("utf-8")
-ubuntu_version = subprocess.check_output(["git", "describe", "--tags", "--always", "--long",
+ubuntu_version = subprocess.check_output(["git", "describe", "--tags", "--always",
         "--match", f"[[:digit:]]*"]).strip().decode("utf-8")
 ubuntu_tag = subprocess.check_output(["git", "describe", "--tags", "--always", "--abbrev=0",
         "--match", f"[[:digit:]]*"]).strip().decode("utf-8")
