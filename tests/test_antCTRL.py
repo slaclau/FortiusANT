d254a7bde99e7bdb2e09d34acb9bf1f17cf74a56
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-17 15:14:06 +0100

    fixes

--- tests/test_antCTRL.py
+++ tests/test_antCTRL.py
@@ -3,5 +3,5 @@ from fortius_ant import antCTRL
 def test_antCTRL(data_regression):
     antCTRL.Initialize()
     for i in range(0,500):
-        output = antCTRL.BroadcastMessage()
+        output = antCTRL.BroadcastControlMessage()
         data_regression.check(output)
