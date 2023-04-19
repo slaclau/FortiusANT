d48eeb5a45ab1f6df7cf558b520cd43b4a7e3d40
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-19 22:14:43 +0100

    Create version.py for json api

---
+++ scripts/json-apis/version.py
@@ -0,0 +1,12 @@
+import json
+from fortius_ant import __version__
+
+schema = {
+    "schemaVersion": 1,
+    "label": "Version",
+    "message": __version__
+    "color": "blue"
+}
+
+response = json.dumps(x)
+print(response)
