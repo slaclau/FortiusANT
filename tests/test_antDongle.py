3a645f37d3d742877ee8ecf5844da5a6fa4083b9
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-19 15:21:53 +0100

    Add fake device

--- tests/test_antDongle.py
+++ tests/test_antDongle.py
@@ -1,7 +1,13 @@
 from fortius_ant import antDongle
+from usb.core import Device
 
 def test_get_devices(mocker):
     mocker.patch("usb.core.find",return_value=[])
     ant_dongle = antDongle.clsAntDongle()
     print(vars(ant_dongle))
-    assert False
+    assert ant_dongle.devAntDongle == None
+    
+    mocker.patch("usb.core.find",return_value=Device("dev",None))
+    ant_dongle = antDongle.clsAntDongle()
+    print(vars(ant_dongle))
+    assert ant_dongle.devAntDongle == None
