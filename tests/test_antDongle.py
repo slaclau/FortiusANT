ea9c61fb764b851a0fd6e9cc11ad6affde040271
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-19 14:47:44 +0100

    Fix test_antDongle.py

--- tests/test_antDongle.py
+++ tests/test_antDongle.py
@@ -3,6 +3,5 @@ from fortius_ant import antDongle
 def test_get_devices(mocker):
     mocker.patch("usb.core.find",return_value=[])
     ant_dongle = antDongle.clsAntDongle()
-    ant_dongle.__GetDongle()
     print(ant_dongle.getmembers())
     assert False
