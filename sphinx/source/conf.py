193fa33eda396acbd25f0edf109a17c2916d5c00
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-19 09:17:55 +0100

    Fix sphinx

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -64,7 +64,7 @@ sys.path.insert(0, os.path.abspath("../"))
 html_theme = "pydata_sphinx_theme"
 # html_theme = 'sphinx_rtd_theme'
 html_static_path = ["_static"]
-html_extra_path = ["coverage"]
+html_extra_path = ["prebuilt"]
 
 html_theme_options = {
     "logo": {
