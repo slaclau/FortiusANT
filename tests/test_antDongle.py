a60fcfda28eafe09de53da711306b5aa65d6be02
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-19 14:33:32 +0100

    Add 'mocker' to test_antDongle.py

--- tests/test_antDongle.py
+++ tests/test_antDongle.py
@@ -1,6 +1,6 @@
 import fortius_ant.antDongle
 
-def test_get_devices():
+def test_get_devices(mocker):
     mocker.patch("usb.core.find",return_value=[])
     ant_dongle = clsAntDongle()
     assert True
