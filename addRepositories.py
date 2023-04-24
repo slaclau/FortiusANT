d9eafa80d5689216100843a02c407b12d2a14859
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-24 12:06:58 +0100

    Try this

--- addRepositories.py
+++ addRepositories.py
@@ -11,5 +11,6 @@ if codename == "focal":
 v = sys.version_info.minor
 
 if v > 9:
-    subprocess.run("echo 'deb https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu jammy main' >> /etc/apt/sources.list")
+    with open("/etc/apt/sources.list", "a") as file:
+        file.write("deb https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu jammy main")
     subprocess.run("apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F23C5A6CF475977595C89F51BA6932366A755776")
