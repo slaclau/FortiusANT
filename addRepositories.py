c10c7a533eba9691659234f40a44cc792139e75b
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-26 13:25:09 +0100

    Create addRepositories.py

---
+++ addRepositories.py
@@ -0,0 +1,16 @@
+import distro
+import fileinput
+import subprocess
+import sys
+
+codename = distro.codename()
+
+if codename == "focal":
+    subprocess.run(["add-apt-repository", "ppa:jyrki-pulliainen/dh-virtualenv"])
+
+v = sys.version_info.minor
+
+if v > 9:
+    with open("/etc/apt/sources.list", "a") as file:
+        file.write("deb https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu jammy main")
+    subprocess.run(["apt-key", "adv", "--keyserver", "keyserver.ubuntu.com", "--recv-keys",     "F23C5A6CF475977595C89F51BA6932366A755776"])
